1e2c6fd969873029e8b0e9c9338a29c775159d74
Doug <27059467+doug-newton@users.noreply.github.com>
2023-07-29 12:39:38 +0200

    display list of dummy posts

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -6,5 +6,9 @@ import { Component } from "@angular/core";
     styleUrls: ['./post-list.component.scss']
 })
 export class PostListComponent {
-
+    posts = [
+        { title: 'First Post', content: 'this is first post\'s content' },
+        { title: 'Second Post', content: 'this is second post\'s content' },
+        { title: 'Third Post', content: 'this is third post\'s content' }
+    ]
 }
