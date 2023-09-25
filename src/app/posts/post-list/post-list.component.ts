d6db87603d7e7ae17a55a8ed31857157e9cfa888
Doug <27059467+doug-newton@users.noreply.github.com>
2023-09-25 11:39:52 +0200

    add paginator with dummy values

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from "@angular/core";
 import { Post } from "../post.model";
 import { PostsService } from "../posts.service";
+import { PageEvent } from "@angular/material/paginator";
 
 @Component({
     selector: 'app-post-list',
@@ -11,6 +12,10 @@ export class PostListComponent implements OnInit {
 
     constructor(public postsService: PostsService) { }
 
+    totalPosts: number = 10
+    postsPerPage: number = 2
+    pageSizeOptions = [1, 2, 5, 10]
+
     public posts$ = this.postsService.posts$
 
     ngOnInit(): void {
@@ -19,5 +24,9 @@ export class PostListComponent implements OnInit {
 
     onDelete(postId: string) {
         this.postsService.deletePost(postId)
+    }
+
+    onChangePage(pageData: PageEvent) {
+        console.log(pageData)
     }
 }
