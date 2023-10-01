489c26607986a1cd1a8d61496a296fabed9a5fe2
Doug <27059467+doug-newton@users.noreply.github.com>
2023-10-01 13:17:41 +0200

    fix typo

--- backend/middleware/check-auth.js
+++ backend/middleware/check-auth.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken')
 module.exports = (req, res, next) => {
     try {
         const token = req.headers.authorization.split(' ')[1]
-        jwt.verify(process.env.JWT_SECRET)
+        jwt.verify(token, process.env.JWT_SECRET)
         next()
     }
     catch (error) {
