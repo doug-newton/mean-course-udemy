c2cd181f65e34bac66cd95c424fdef7b031788fe
Doug <27059467+doug-newton@users.noreply.github.com>
2023-12-05 21:38:35 +0200

    store decoded userData from token in request

--- backend/middleware/check-auth.js
+++ backend/middleware/check-auth.js
@@ -3,7 +3,8 @@ const jwt = require('jsonwebtoken')
 module.exports = (req, res, next) => {
     try {
         const token = req.headers.authorization.split(' ')[1]
-        jwt.verify(token, process.env.JWT_SECRET)
+        const userData = jwt.verify(token, process.env.JWT_SECRET)
+        req.userData = userData
         next()
     }
     catch (error) {
